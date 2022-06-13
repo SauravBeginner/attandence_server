1db2c3c56027f841a680419a81181d6c5ea3a0f3
Saurav <kundusaurav1996@gmail.com>
2022-06-13 23:50:14 +0530

    database connection

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
-const PORT = 5000;
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Database connected!`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
