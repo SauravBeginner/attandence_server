fb2212b13a7a46b9242dff16762fd1e201849454
Saurav <kundusaurav1996@gmail.com>
2022-06-13 23:27:54 +0530

    server running

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
