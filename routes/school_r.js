21e6fcd62082d8bd0f978b992ac484a624e457b1
Saurav <kundusaurav1996@gmail.com>
2022-06-15 02:03:20 +0530

    role authorization done

--- routes/school_r.js
+++ routes/school_r.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const School = require("../models/School");
+const verify = require("./verifyToken");
 
-router.post("/add", async (req, res) => {
-  const newSchool = new School(req.body);
-  try {
-    const addedSchool = await newSchool.save();
-    console.log(addedSchool);
-    res.status(200).json(addedSchool);
-  } catch (err) {
-    res.status(500).json(err);
-    console.log(err);
+//ADD SCHOOL
+router.post("/add", verify, async (req, res) => {
+  if (req.user.role === "admin") {
+    const newSchool = new School(req.body);
+    try {
+      const addedSchool = await newSchool.save();
+      console.log(addedSchool);
+      res.status(200).json(addedSchool);
+    } catch (err) {
+      res.status(500).json(err);
+      console.log(err);
+    }
+  } else {
+    console.log(`You are not authorizd!`);
+    res.status(403).json({ err: `You are not authorizd!` });
   }
 });
 
